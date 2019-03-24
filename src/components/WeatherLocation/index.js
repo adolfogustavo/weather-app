50dd0a3d75c2f4527ceb5d491d88db6caca21e0e
Gustavo <gustavodominguez230@gmail.com>
2019-03-24 18:44:22 -0300

    Probando actualización de states

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
@@ -17,12 +17,41 @@ const data = {
     humidity: 80,
     wind: '10 m/s',
 }
+const data2 = {
+    temperature: 15,
+    weatherState: WINDY,
+    humidity: 10,
+    wind: '30 m/s',
+}
 
-const WeatherLocation = () => (
-    <div className="weatherLocationCont">
-        <Location city={"Santiago"}></Location>
-        <WeatherData data={data}></WeatherData>
-    </div>
-);
+class WeatherLocation extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            city: 'Santiago',
+            data: data,
+        };
+    }
+
+    handleUpdateClick = () => {
+        console.log("Actualizado");
+
+        this.setState({
+            data: data2
+        });
+    }
+    
+    render(){
+        const { city, data } = this.state;
+        return (
+            <div className="weatherLocationCont">
+                <Location city={city}></Location>
+                <WeatherData data={data}></WeatherData>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
+            </div>
+        );
+    } 
+}
 
 export default WeatherLocation;
