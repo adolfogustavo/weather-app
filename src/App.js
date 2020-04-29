332cb6a5ecc7ef046b8016db505988f769c0efbc
Gustavo Dominguez <gustavodominguez230@gmail.com>
2020-04-29 12:16:35 -0400

    ADD: Componentes forecastExtended para ver en detalle el pronóstico extendido de un país

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from '@material-ui/core';
+import { Grid, Col, Row } from 'react-flexbox-grid';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Grid, Col, Row } from 'react-flexbox-grid';
 import LocationList from './components/LocationList';
+import ForecastExtended from './components/ForecastExtended';
+
 import './App.css';
 
 const cities = [
@@ -17,36 +20,52 @@ const cities = [
 ]
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: null,
+    }
+  }
 
   handleSelectedLocation = city => {
+    this.setState({ city })
     console.log(`handleSelectionLocation ${city}`);
   }
 
   render() {
+    const { city } = this.state;
     return (
-      <Grid>
-        <Row>
-          <AppBar position='sticky'>
-            <Toolbar>
-              <Typography variant='title' color='inherit'>
-                Weather App
-              </Typography>
-            </Toolbar>
-          </AppBar>
-        </Row>
-        <Row>
-          <Col xs={12} md={6}>
-            <LocationList cities={cities}
-              onSelectedLocation={this.handleSelectedLocation}>
-            </LocationList>
-          </Col>
-          <Col xs={12} md={6}>
-            <Paper>
-              <div className="details"></div>
-            </Paper>
-          </Col>
-        </Row>
-      </Grid>
+      <MuiThemeProvider>
+        <Grid>
+          <Row>
+            <AppBar position='sticky'>
+              <Toolbar>
+                <Typography variant='title' color='inherit'>
+                  Weather App
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList cities={cities}
+                onSelectedLocation={this.handleSelectedLocation}>
+              </LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+              <Paper elevation={4}>
+                <div className="details">
+                  { 
+                    !city ? 
+                    <h1>No se seleccionó ninguna ciudad</h1> : 
+                    <ForecastExtended city={city} ></ForecastExtended>
+                  }
+                </div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/components/ForecastItem/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import WeatherData from './../WeatherLocation/WeatherData';
+
+const ForecastItem = ({ weekDay, hour, data }) => (
+    <div>
+        <h2>{weekDay} - {hour} hrs</h2>
+        <WeatherData data={data} ></WeatherData>
+    </div>
+);
+
+ForecastItem.propTypes = {
+    weekDay: PropTypes.string.isRequired,
+    hour: PropTypes.number.isRequired,
+}
+
+export default ForecastItem;
