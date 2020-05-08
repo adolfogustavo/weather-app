73aeb833497f455d794a207ab9e74b8713fcc4f9
Gustavo Dominguez <gustavodominguez@MacBook-Air-de-Gustavo.local>
2020-05-08 16:57:45 -0400

    ADD: Incorporación del estado global con store a través de redux y react-redux

---
+++ src/reducers/city.js
@@ -0,0 +1,13 @@
+import { SET_CITY } from './../actions';
+
+// Si el state nos llegara a venir en nulo, con esta convención nos aseguramos de asignar un valor por defecto state = {}, en lugar de state sólo
+export const city = (state = {}, action) => {
+    console.log('state=>', state);
+    console.log('action=>', action);
+    switch(action.type) {
+        case SET_CITY:
+            return { ...state, city: action.payload }
+        default:
+            return state
+    }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,13 @@
 import { createStore } from 'redux';
+import { city } from './../reducers/city';
 
-export const store = createStore(() => {}, 
+const initialState = {
+    city: 'Santiago,cl'
+};
+
+
+// Primer parámetro: reducer
+// Segundo parámetro: estado inicial
+// Tercer parámetro (opcional): debug
+export const store = createStore(city, initialState, 
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
